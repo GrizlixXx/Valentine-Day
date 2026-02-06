df0eee09d46de9195bd68691316f3e912d230819
Sumar Illya <illasumar002@gmail.com>
2026-02-06 17:43:26 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
-export default {
-  content: ['./*.html'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // усі html файли в корені
+    './src/**/*.{html,js}', // якщо є папка src
+  ],
   theme: {
     extend: {},
   },
+  plugins: [],
 };
